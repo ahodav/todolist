0628ac398a521c4d5a69c19eed1a98a3f22824ae
ahodav <orest97@ukr.net>
2020-04-13 20:05:58 +0300

    Add 'todolistahodav/dist/' from commit 'fd392263a6d9b23c75fd69d7cbacdf31218849d6'
    git-subtree-dir: todolistahodav/dist
    git-subtree-mainline: fd392263a6d9b23c75fd69d7cbacdf31218849d6
    git-subtree-split: fd392263a6d9b23c75fd69d7cbacdf31218849d6

---
+++ todolistahodav/dist/src/components/AddTodo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="add_btn-wrap">
+    <div class="error">{{error}}</div>
+    <div class="input-wrap">
+      <input class="custon-input" type="text" v-model="nameOfList">
+    </div>
+    <button @click="addTodoList" class="add_btn">Add Todo List </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      nameOfList:'',
+      error:''
+    }
+  },
+  methods: {
+    addTodoList() {
+      this.error = ''
+      console.thi
+      if(this.nameOfList.length < 2){
+        this.error = 'Name of list must contain 2 or more symbols'
+        return
+      }
+      this.$emit('addList', this.nameOfList)
+      this.nameOfList = ''
+    }
+  }
+}
+</script>
+
+<style>
+  .add_btn-wrap {
+    margin: 32px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 30%;
+  }
+
+  .custon-input {
+    width: 100%;
+    height: 32px;
+    border: 1px solid gray;
+    border-radius: 8px;
+  }
+
+  .add_btn {
+    color: white;
+    background: blue;
+    height: 32px;
+    border-radius: 8px;
+    border: 1px solid lawngreen;
+    cursor: pointer;
+  }
+
+  .error {
+    color: red;
+    margin: 4px;
+  }
+</style>
---
+++ todolistahodav/dist/src/components/Banner.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="banner" :class="{active: isActive}">
+    <span>Delete element "{{item.name}}"</span>
+    <button @click="yes" class="btn btn-yes">Yes</button>
+    <button @click="no" class="btn btn-no">No</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['item', 'isActive'],
+  data() {
+    return {
+    }
+  },
+  created(){
+  },
+  methods: {
+    yes() {
+      this.$emit('remove', this.item.id)
+    },
+    no() {
+      this.$emit("cancel")
+    }
+  }
+}
+</script>
+
+<style>
+  .banner {
+    height: 38px;
+    width: 100%;
+    position: fixed;
+    background: green;
+    top:0;
+    left: 0;
+    display: none;
+    align-items: center;
+    justify-content: center;
+    color:white
+  }
+  .active{
+    display: flex;
+  }
+
+  .btn {
+    margin: 0 8px;
+  }
+
+  .btn-yes {
+    width: 32px;
+    height: 24px;
+    background: #03fc2c;
+  }
+  .btn-no {
+    width: 32px;
+    height: 24px;
+    background: red;
+  }
+</style>
