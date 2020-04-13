70a136e967e41ab78f3ab4851903448cb3da7d78
ahodav <orest97@ukr.net>
2020-04-13 19:41:55 +0300

    add static.json nazik

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,11 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
-app.use(serveStatic(__dirname))
+// app.use(serveStatic(__dirname))
 var port = process.env.PORT || 5000
+app.use(express.static(__dirname + '/api/front'));
+app.get(/.*/, (req, res) =>{
+    res.sendFile(__dirname + '/index.html')
+});
 app.listen(port)
 console.log('server started ' + port)
