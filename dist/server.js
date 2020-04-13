36cd4c691810fdd7c129a57a8649be369b92af4e
ahodav <orest97@ukr.net>
2020-04-13 19:51:16 +0300

    remove statick

--- dist/server.js
+++ dist/server.js
@@ -2,11 +2,12 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 app = express()
-// app.use(serveStatic(__dirname))
-var port = process.env.PORT || 5000
-app.use(express.static(__dirname + '/api/front'));
+app.use(serveStatic(__dirname))
+// app.use(express.static(__dirname + '/api/front'));
 app.get(/.*/, (req, res) =>{
     res.sendFile(__dirname + '/index.html')
 });
+var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
+console.log(__dirname)
